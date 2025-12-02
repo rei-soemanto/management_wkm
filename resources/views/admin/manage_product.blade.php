f993cb17d1da5a6001b982ed34ee4fc409f81092
rei-soemanto <reresoemanto@gmail.com>
2025-12-02 22:12:21 +0700

    fix feature hidden and manager role 6

--- resources/views/admin/manage_product.blade.php
+++ resources/views/admin/manage_product.blade.php
@@ -137,6 +137,7 @@
                                 <th scope="col" class="px-6 py-4 text-left text-sm font-bold uppercase tracking-wider">Brand</th>
                                 <th scope="col" class="px-6 py-4 text-left text-sm font-bold uppercase tracking-wider">Category</th>
                                 <th scope="col" class="px-6 py-4 text-left text-sm font-bold uppercase tracking-wider hidden md:table-cell">Last Updated</th>
+                                <th scope="col" class="px-6 py-4 text-left text-sm font-bold uppercase tracking-wider hidden md:table-cell">Hidden</th>
                                 <th scope="col" class="px-6 py-4 text-center text-sm font-bold uppercase tracking-wider">Actions</th>
                             </tr>
                         </thead>
@@ -151,12 +152,13 @@
                                         <span class="text-xs">{{ $product->updated_at->format('M j, Y, g:i a') }}</span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-900">
-                                        {{ $product->name }}
                                         @if($product->is_hidden)
                                             <span class="ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-gray-600 text-white">
                                                 <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg>
                                                 Hidden
                                             </span>
+                                        @else
+                                            -
                                         @endif
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
