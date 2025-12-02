63756f1f9585fc69a17c3292a7c4b6e9a3fcefb9
rei-soemanto <reresoemanto@gmail.com>
2025-12-02 09:46:51 +0700

    fixed email test 6

--- routes/console.php
+++ routes/console.php
@@ -8,9 +8,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('tasks:send-reminders')
-    ->dailyAt('09:40')
-    ->timezone('Asia/Jakarta');
+Schedule::command('tasks:send-reminders')->everyMinute();
+    // ->dailyAt('09:40')
+    // ->timezone('Asia/Jakarta');
 
 Schedule::command('admin:send-digest')
     ->dailyAt('20:00')
