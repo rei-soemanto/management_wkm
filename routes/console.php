08c64541a7d8eed0909a93716a377d01b58a8f71
rei-soemanto <reresoemanto@gmail.com>
2025-12-02 12:00:12 +0700

    fixed email test 13

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
 $phpBin = '/usr/local/bin/ea-php82';
 $artisanFile = base_path('artisan');
@@ -11,12 +12,23 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('tasks:send-reminders')->everyMinute()
-    // ->dailyAt('09:00')
-    // ->timezone('Asia/Jakarta')
-    ->appendOutputTo(storage_path('logs/scheduler_output.log'));
+Schedule::command('tasks:send-reminders')
+    ->everyMinute()
+    ->appendOutputTo(storage_path('logs/scheduler_output.log'))
+    ->before(function () {
+        Log::info('tasks:send-reminders scheduled to run at ' . now());
+    })
+    ->after(function () {
+        Log::info('tasks:send-reminders finished at ' . now());
+    });
 
 Schedule::command('admin:send-digest')
     ->dailyAt('20:00')
     ->timezone('Asia/Jakarta')
-    ->appendOutputTo(storage_path('logs/scheduler_output.log'));
+    ->appendOutputTo(storage_path('logs/scheduler_output.log'))
+    ->before(function () {
+        Log::info('admin:send-digest scheduled to run at ' . now());
+    })
+    ->after(function () {
+        Log::info('admin:send-digest finished at ' . now());
+    });
